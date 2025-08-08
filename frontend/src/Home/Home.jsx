b973bc7406aabb21420101c33e31c968457c03aa
anhb8 <bichtram1307@gmail.com>
2025-08-08 14:30:31 -0500

    Embed Typeform form to the website

---
+++ frontend/src/Home/Home.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import landingImage from '../images/landing-page.png';
+import './Home.css';
+import { useNavigate } from 'react-router-dom';
+import { createPopup } from '@typeform/embed';
+import '@typeform/embed/build/css/popup.css';
+
+const Home = () => {
+  const navigate = useNavigate();
+  
+  const openForm = () => {
+    createPopup('bjiD63Uy', {
+      mode: 'popup',   
+      size: 100,     
+      autoClose: 0     
+    }).open();
+  };
+
+  return (
+    <div className="landing-wrapper">
+      <nav className="navbar">
+        <div className="logo">PathNova</div>
+        <div className="nav-links">
+          <span className="nav-link inactive">Overview</span>
+          <span className="nav-link active">Features</span>
+          <span className="nav-link">FAQ</span>
+          <button className="signin-btn">Sign In</button>
+        </div>
+      </nav>
+
+      <main className="content">
+        <div className="left">
+          <h1 className="title">
+            Your Personalized Career Coach – Tailored Guidance to Land Your
+            Dream Job
+          </h1>
+          <p className="subtitle">
+            Get custom study paths, project ideas, job tracking, and expert
+            resources based on your goals
+          </p>
+          
+          <button className="start-btn" onClick={openForm}>Start</button>
+        </div>
+        <div className="right">
+          <img src={landingImage} alt="Career Coach" />
+        </div>
+      </main>
+    </div>
+  );
+};
+export default Home;
---
+++ frontend/src/TypeForm/TypeForm.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function TypeForm() {
+    return (
+        
+      <div className="text-2xl p-10">
+        <div>Hello </div>
+        <div data-tf-live="01K20NTYAXAK0S97HTNCM0PR6M"></div><script src="//embed.typeform.com/next/embed.js"></script>
+      </div>
+    );
+  }
+  
+  export default TypeForm;
+  
